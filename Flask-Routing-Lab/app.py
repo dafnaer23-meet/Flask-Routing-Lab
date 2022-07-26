7808884d0347cdd94bbccb01b53de611b2226224
dafnaer23-meet <109908294+dafnaer23-meet@users.noreply.github.com>
2022-07-26 16:33:51 +0300

    Update app.py

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -8,15 +8,15 @@ app = Flask(  # Create a flask app
 )
 @app.route('/')
 def home():
-   return render_template(hello.html)
+   return render_template("hello.html")
 
 @app.route('/product')
 def product():
-   return render_template(product.html)
+   return render_template("product.html")
 
 @app.route('/cart')
 def cart():
-   return render_template(cart.html)
+   return render_template("cart.html")
 
 # Your code should be below
 
